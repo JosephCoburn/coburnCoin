d328eba94f62e557305e0407006d8e47e632ca69
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-08-14 20:10:04 -0700

    add basic proof of work

--- main.js
+++ main.js
@@ -7,16 +7,29 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
+      this.nonce++;
+      this.hash=this.calculateHash();
+    } 
+
+    console.log("Block mined: " + this.hash);
   }
 }
+
+
 
 class Blockchain{
   constructor(){
     this.chain=[this.createGenesisBlock()];
+    this.difficulty=4;
   }
 
   createGenesisBlock() {
@@ -29,13 +42,33 @@ class Blockchain{
 
   addBlock(newBlock){
     newBlock.previousHash=this.getLatestBlock().hash;
-    newBlock.hash=newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for(let i=1;i<this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        console.log("!!! A block's hash isn't the actual SHA256...", currentBlock.hash, currentBlock.calculateHash())
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        console.log("!!! A block's 'previous hash' value doesnt match the previous block's actual hash... ", currentBlock.previousHash, previousBlock.calculateHash());
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
 let coburnCoin = new Blockchain();
-coburnCoin.addBlock(new Block(1, "01/04/2009", { amount: 4 }))
-coburnCoin.addBlock(new Block(2, "01/05/2009", { amount: 10 }))
 
-console.log(JSON.stringify(coburnCoin, null, 4));
+console.log("Mining block 1...");
+coburnCoin.addBlock(new Block(1, "01/04/2009", { amount: 4 }))
+console.log("Mining block 2...");
+coburnCoin.addBlock(new Block(2, "01/05/2009", { amount: 10 }))
