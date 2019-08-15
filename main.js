534637b7901f132edb4d7a4c00881320d937a642
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-08-14 21:07:39 -0700

    Add signing

--- main.js
+++ main.js
@@ -1,118 +1,21 @@
-const SHA256 = require('crypto-js/sha256');
+const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction{
-  constructor(fromAddress, toAddress, amount) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
-    this.amount = amount;
-  }
-}
-
-class Block {
-  constructor(timestamp, transactions, previousHash='') {
-    this.previousHash = previousHash;
-    this.timestamp = timestamp;
-    this.transactions = transactions;
-    this.hash = this.calculateHash();
-    this.nonce = 0;
-  }
-
-  calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
-  }
-
-  mineBlock(difficulty) {
-    while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")){
-      this.nonce++;
-      this.hash=this.calculateHash();
-    } 
-
-    console.log("Block mined: " + this.hash);
-  }
-}
-
-
-
-class Blockchain{
-  constructor(){
-    this.chain=[this.createGenesisBlock()];
-    this.difficulty=2;
-    this.pendingTransactions=[];
-    this.miningReward=100;
-  }
-
-  createGenesisBlock() {
-    return new Block("01/03/2009", "Genesis block", "0");
-  }
-
-  getLatestBlock(){
-    return this.chain[this.chain.length-1];
-  }
-
-  minePendingTransactions(miningRewardAddress) {
-    let block=new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
-    block.mineBlock(this.difficulty);
-
-    console.log("block successfully mined");
-    this.chain.push(block);
-
-    this.pendingTransactions=[
-      new Transaction(null, miningRewardAddress, this.miningReward)
-    ];
-  }
-
-  createTransaction(transaction) {
-    this.pendingTransactions.push(transaction);
-  }
-
-  getBalanceOfAddress(address) {
-    let balance=0;
-    for(const block of this.chain){
-      for(const trans of block.transactions){
-        if(trans.fromAddress === address){
-          balance -= trans.amount;
-        }
-
-        if(trans.toAddress === address){
-          balance += trans.amount;
-        }
-      }
-    }
-
-    return balance;
-  }
-
-  isChainValid() {
-    for(let i=1;i<this.chain.length; i++) {
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i-1];
-
-      if(currentBlock.hash !== currentBlock.calculateHash()) {
-        console.log("!!! A block's hash isn't the actual SHA256...", currentBlock.hash, currentBlock.calculateHash())
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        console.log("!!! A block's 'previous hash' value doesnt match the previous block's actual hash... ", currentBlock.previousHash, previousBlock.calculateHash());
-        return false;
-      }
-    }
-
-    return true;
-  }
-}
+const myKey = ec.keyFromPrivate('c6bbd0103d7ecbed596e61d20c137eaa75767e4a6ff74fdffa34febf2f004a12');
+const myWalletAddress = myKey.getPublic('hex');
 
 let coburnCoin = new Blockchain();
 
-coburnCoin.createTransaction(new Transaction('address 1', 'address 2', 100));
-coburnCoin.createTransaction(new Transaction('address 2', 'address 1', 50));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+coburnCoin.addTransaction(tx1);
 
 console.log('\n Starting the miner...');
-coburnCoin.minePendingTransactions('joes-address');
+coburnCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\n Balance of joe is', coburnCoin.getBalanceOfAddress('joes-address'));
+console.log('\n Balance of joe is', coburnCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\n Starting the miner...');
-coburnCoin.minePendingTransactions('joes-address');
+console.log('is chain valid?', coburnCoin.isChainValid());
 
-console.log('\n Balance of joe is', coburnCoin.getBalanceOfAddress('joes-address'));
+// Public key:  049315cb72c60523daf8d51cbd01b7bda384a51f6f3a1fadb378b30362e5b7a666ef2013a912dd80da025c0d7cee4007d9ea6ad75a7efeb42a2219d041a878109c
